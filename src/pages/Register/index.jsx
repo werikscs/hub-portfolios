007cc880d95cdeeb1f9d08418df3d2383e14a42f
Weriks Santos <you@example.comwerikscs@gmail.com>
2022-04-22 19:09:09 -0300

    feat(pages/register): add jsx content to Register page

---
+++ src/pages/Register/index.jsx
@@ -0,0 +1,37 @@
+import Button from "../../components/Button";
+import InputPassword from "../../components/InputPassword";
+import InputText from "../../components/InputText";
+import LogoKenzie from "../../components/LogoKenzie";
+import * as S from "./styles";
+
+const Register = () => {
+  return (
+    <S.Section>
+      <section>
+        <LogoKenzie />
+        <button>voltar</button>
+      </section>
+
+      <S.Div>
+        <h2>Crie sua conta</h2>
+
+        <span>Rápido e grátis, vamos nessa</span>
+
+        <InputText label="Nome" placeholder="Digite seu nome" />
+
+        <InputText label="Email" placeholder="Digite seu email" />
+
+        <InputPassword label="Senha" placeholder="Digite sua senha" />
+
+        <InputPassword
+          label="Confirmar Senha"
+          placeholder="Digite novamente sua senha"
+        />
+
+        <Button text="Cadastrar" colorType="negative" />
+      </S.Div>
+    </S.Section>
+  );
+};
+
+export default Register;
