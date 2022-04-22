6f5daa2a022c6bbc8434f130d6752e572f8a0419
Weriks Santos <you@example.comwerikscs@gmail.com>
2022-04-22 13:45:35 -0300

    feat(src/pages/login): add basic html and basic styled-component

---
+++ src/pages/Login/index.jsx
@@ -0,0 +1,37 @@
+const Login = () => {
+
+  return(
+
+    <section>
+
+      <h1>Kenzie Hub</h1>
+
+      <div>
+
+        <h2>Login</h2>
+
+        <div>
+          <div>Email</div>
+          <input type="text" placeholder="Email"/>
+        </div>
+
+        <div>
+          <div>Email</div>
+          <input type="text" placeholder="Senha"/>
+        </div>
+
+        <button>Entrar</button>
+
+        <span>Ainda não possui uma conta?</span>
+
+        <button>Cadastre-se</button>
+
+      </div>
+
+    </section>
+
+  );
+
+}
+
+export default Login;
