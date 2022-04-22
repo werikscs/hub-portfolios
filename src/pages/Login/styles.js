b98116f6e09282da5ce3bb9fb089eb1cb3acfb96
Weriks Santos <you@example.comwerikscs@gmail.com>
2022-04-22 15:33:54 -0300

    feat(src/pages/login/styles.js): add more styles

--- src/pages/Login/styles.js
+++ src/pages/Login/styles.js
@@ -6,10 +6,10 @@ export const Section = styled.section`
   align-items: center;
 
   width: 100%;
-  min-width: 300px;
+  min-width: 320px;
   max-width: 370px;
 
-  background-color: var(--color-gray-3);
+  padding: 0 12px;
 `;
 
 export const Div = styled.div`
@@ -18,4 +18,17 @@ export const Div = styled.div`
   align-items: center;
 
   width: 100%;
+
+  padding: 0 14px;
+
+  border-radius: 3px;
+
+  background-color: var(--color-gray-3);
+
+  h2 {
+    margin-top: 34px;
+    margin-bottom: 20px;
+
+    font-size: 0.9rem;
+  }
 `;
