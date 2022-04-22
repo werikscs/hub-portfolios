974895dd38c54288d17109dbc776f19ad4de3ac1
Weriks Santos <you@example.comwerikscs@gmail.com>
2022-04-22 14:31:44 -0300

    feat(src/pages/login/styles.js): add styles to Section styled component and add Div styled component

--- src/pages/Login/styles.js
+++ src/pages/Login/styles.js
@@ -1,6 +1,21 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 export const Section = styled.section`
   display: flex;
-  
-`
+  flex-direction: column;
+  align-items: center;
+
+  width: 100%;
+  min-width: 300px;
+  max-width: 370px;
+
+  background-color: var(--color-gray-3);
+`;
+
+export const Div = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  width: 100%;
+`;
