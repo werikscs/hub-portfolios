fb027556d3f13f5863f7ee352c5f2f9685a1f569
Weriks Santos <you@example.comwerikscs@gmail.com>
2022-04-22 19:04:44 -0300

    feat(src/routes): add Register component to path="/register"

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,31 +1,24 @@
 import { useState } from "react";
 import { Switch, Route } from "react-router-dom";
 import Login from "../pages/Login";
+import Register from "../pages/Register";
 
 const Routes = () => {
-
   const [isAuthenticated, setIsAuthenticated] = useState(false);
 
   return (
-
     <Switch>
-
-      <Route exact path='/'>
-        <Login/>
+      <Route exact path="/">
+        <Login />
       </Route>
 
-      <Route path='/register'>
-        {/* register */}
+      <Route path="/register">
+        <Register />
       </Route>
 
-      <Route path='/dashboard'>
-        {/* dashboard */}
-      </Route>
-
+      <Route path="/dashboard">{/* dashboard */}</Route>
     </Switch>
-
   );
-
-}
+};
 
 export default Routes;
