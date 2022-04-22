13725ef5977af5f5ada9a0a5fcf3b17d17108aa0
Weriks Santos <you@example.comwerikscs@gmail.com>
2022-04-22 11:54:19 -0300

    feat(src/app.js): add Routes component to App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import Routes from './routes';
 import GlobalStyles from './styles/global';
 
 function App() {
   return (
     <>
       <GlobalStyles/>
+      <Routes/>
     </>
   );
 }
