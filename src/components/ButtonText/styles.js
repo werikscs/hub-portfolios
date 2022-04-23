a657046e9895fb9687e2255df19b291cf5e2cba5
Weriks Santos <you@example.comwerikscs@gmail.com>
2022-04-22 23:23:16 -0300

    refactor(components/buttontext): change Buttontext style to accept diferents font-size

--- src/components/ButtonText/styles.js
+++ src/components/ButtonText/styles.js
@@ -3,9 +3,9 @@ import styled from "styled-components";
 export const Button = styled.button`
   width: fit-content;
 
-  padding: 8px 24px;
+  padding: 8px 16px;
 
-  font-size: 0.75rem;
+  font-size: ${({ fontSize }) => (fontSize ? fontSize : "0.75rem")};
   font-weight: 600;
 
   border-radius: 4px;
