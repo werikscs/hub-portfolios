be603d159e7750e5bc80b5cbb047717dd9fa101a
Weriks Santos <you@example.comwerikscs@gmail.com>
2022-04-22 22:10:03 -0300

    feat(src/routes): add Dashboard component to route '/dashboard'

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { Switch, Route } from "react-router-dom";
+import Dashboard from "../pages/Dashboard";
 import Login from "../pages/Login";
 import Register from "../pages/Register";
 
@@ -16,7 +17,9 @@ const Routes = () => {
         <Register />
       </Route>
 
-      <Route path="/dashboard">{/* dashboard */}</Route>
+      <Route path="/dashboard">
+        <Dashboard />
+      </Route>
     </Switch>
   );
 };
