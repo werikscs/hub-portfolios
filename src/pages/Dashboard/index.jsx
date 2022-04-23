5c525323aecf8cdbfe77190c71414dc5c47e1160
Weriks Santos <you@example.comwerikscs@gmail.com>
2022-04-23 14:20:31 -0300

    feat(pages/dashboard): add modal component do Dashboard page

--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -1,11 +1,33 @@
 import ButtonText from "../../components/ButtonText";
 import Header from "../../components/Header";
 import LogoKenzie from "../../components/LogoKenzie";
+import Modal from "../../components/Modal";
 import * as S from "./styles";
 
 const Dashboard = () => {
   return (
     <S.Section>
+      {/* <Modal
+        text="Cadastrar Tecnologia"
+        inputText="Nome"
+        inputTextPlaceholder="Nome da Tecnologia"
+        inputSelect="Selecionar Status"
+        options={["Primeiro Módudo", "Segundo Módulo", "Terceiro Módulo"]}
+        buttons={[{ text: "Cadastrar Tecnologia" }]}
+      /> */}
+
+      {/* <Modal
+        text="Detalhes da Tecnologia"
+        inputText="Nome do Projeto"
+        inputTextPlaceholder="Material UI"
+        inputSelect="Status"
+        options={["Iniciante", "Indermediário", "Avançando"]}
+        buttons={[
+          { text: "Salvar Alterações", colorType: "negative" },
+          { text: "Excluir", colorType: "disabled", width: "fit-content" },
+        ]}
+      /> */}
+
       <Header>
         <LogoKenzie />
         <ButtonText text="Sair" />
