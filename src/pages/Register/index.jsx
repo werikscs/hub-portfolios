be3417dc22340acdf8789d7ab90b351e7436ad80
Weriks Santos <you@example.comwerikscs@gmail.com>
2022-04-23 16:23:06 -0300

    feat(pages/register): add form validation to the component pages/Register

--- src/pages/Register/index.jsx
+++ src/pages/Register/index.jsx
@@ -1,4 +1,9 @@
+import { useForm } from "react-hook-form";
 import { useHistory } from "react-router-dom";
+
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+
 import Button from "../../components/Button";
 import ButtonText from "../../components/ButtonText";
 import Header from "../../components/Header";
@@ -6,9 +11,36 @@ import InputPassword from "../../components/InputPassword";
 import InputText from "../../components/InputText";
 import LogoKenzie from "../../components/LogoKenzie";
 import SelectInput from "../../components/SelectInput";
+
 import * as S from "./styles";
+import { useState } from "react";
 
 const Register = () => {
+  const schema = yup.object().shape({
+    name: yup.string().required("Campo obrigatório!"),
+    email: yup.string().required("Campo obrigatório!"),
+    password: yup.string().required("Campo obrigatório!"),
+    passwordConfirm: yup
+      .string()
+      .required("Campo obrigatório!")
+      .oneOf([yup.ref("password")], "As senhas são diferentes!"),
+  });
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({ resolver: yupResolver(schema) });
+
+  const onSubmit = (dataForm) => {
+    console.log(errors);
+    console.log(dataForm, options[select]);
+  };
+
+  const options = ["Primeiro Módulo", "Segundo Módulo", "Terceiro Módulo"];
+
+  const [select, setSelect] = useState(0);
+
   const history = useHistory();
 
   const goToLogin = () => {
@@ -22,29 +54,52 @@ const Register = () => {
         <ButtonText text="Voltar" callback={goToLogin} />
       </Header>
 
-      <S.Div>
+      <S.Form onSubmit={handleSubmit(onSubmit)}>
         <h2>Crie sua conta</h2>
 
-        <span>Rápido e grátis, vamos nessa</span>
+        <span className="register-msg">Rápido e grátis, vamos nessa</span>
 
-        <InputText label="Nome" placeholder="Digite seu nome" />
+        <InputText
+          label="Nome"
+          name="name"
+          register={register}
+          error={errors.name?.message}
+          placeholder="Digite seu nome"
+        />
 
-        <InputText label="Email" placeholder="Digite seu email" />
+        <InputText
+          label="Email"
+          name="email"
+          register={register}
+          error={errors.email?.message}
+          placeholder="Digite seu email"
+        />
 
-        <InputPassword label="Senha" placeholder="Digite sua senha" />
+        <InputPassword
+          label="Senha"
+          name="password"
+          register={register}
+          error={errors.password?.message}
+          placeholder="Digite sua senha"
+        />
 
         <InputPassword
           label="Confirmar Senha"
+          name="passwordConfirm"
+          register={register}
+          error={errors.passwordConfirm?.message}
           placeholder="Digite a senha novamente"
         />
 
         <SelectInput
           label="Selecionar módulo"
-          options={["Primeiro Módulo", "Segundo Módulo", "Terceiro Módulo"]}
+          options={options}
+          select={select}
+          setSelect={setSelect}
         />
 
         <Button text="Cadastrar" colorType="negative" />
-      </S.Div>
+      </S.Form>
     </S.Section>
   );
 };
